2a058c5855b0c1152e291755bfe3677f0acd135e
Виктория <vika_nazarova_05@inbox.ru>
2021-01-31 21:50:31 +0300

    5 commit. Sprites' collid

--- Shape.py
+++ Shape.py
@@ -19,16 +19,17 @@ def load_image(name, size, colorkey=None):
 
 
 all_sprites = pygame.sprite.Group()
+b = pygame.sprite.Group()
 
 
 class Shape:
     def __init__(self, count):
-        border = pygame.sprite.Sprite()
-        border.image = pygame.Surface([500, 1])
-        border.rect = border.image.get_rect()
-        border.rect.x, border.rect.y = 5, 750
-        all_sprites.add(border)
-        self.moving_sprite = Moving_shape(count, border)
+        self.border = pygame.sprite.Sprite()
+        self.border.image = pygame.Surface([500, 1])
+        self.border.rect = self.border.image.get_rect()
+        self.border.rect.x, self.border.rect.y = 5, 750
+        b.add(self.border)
+        self.moving_sprite = Moving_shape(count, self.border)
 
     def render(self, screen):
         all_sprites.draw(screen)
@@ -41,14 +42,17 @@ class Shape:
         self.render(screen)
 
     def click(self, key, screen):
-        if key == pygame.K_RIGHT and self.moving_sprite.rect.x <= 450:
+        if key == pygame.K_RIGHT and self.moving_sprite.rect.right <= 450:
             self.moving_sprite.rect.x += 50
         elif key == pygame.K_LEFT and self.moving_sprite.rect.x >= 50:
             self.moving_sprite.rect.x -= 50
+        elif key == pygame.K_SPACE:
+            self.moving_sprite.image = pygame.transform.rotate(screen, self.moving_sprite.image)
         self.render(screen)
 
     def ckeck_collid(self, screen):
-        if self.moving_sprite.rect.bottom == 800:
+        if pygame.sprite.spritecollideany(self.moving_sprite, b):
+            b.add(self.moving_sprite)
             self.render(screen)
             return True
         return False
@@ -62,7 +66,7 @@ class Moving_shape(pygame.sprite.Sprite):
         self.image = load_image(str(count % 4 + 1) + '.png', size)
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
-        self.rect.x, self.rect.y = 200, 0
+        self.rect.x, self.rect.y = 200, 50 - self.rect.bottom + self.rect.y
 
     def update(self):
         if not pygame.sprite.collide_mask(self, self.border):
